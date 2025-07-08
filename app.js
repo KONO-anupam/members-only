911a815422b37f2c902ce6490612cd4e6390bd02
Kono <anupam.nitrkl@gmail.com>
2025-07-08 18:32:43 +0530

    feat: added sign up form

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStratergy = require('passport-local').Stratergy;
+require('dotenv').config();
+const app = express();
 
 app.set("views",path.join(__dirname, "views"));
 
@@ -10,9 +12,10 @@ app.use(session({secret:process.env.SESSION_SECRET, resave: false, saveUninitial
 app.use(passport.session());
 app.use(express.urlencoded({extended:false}));
 
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','SignUpForm.html'))); 
+
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`the server is listening on ${PORT}`);
 });
-
-const app = express();
